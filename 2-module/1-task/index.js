0b9606dd67375cafdc0e18bf45a806e80637404f
Serghei Nezdvetki <moeapo4ta@gmail.com>
2021-03-10 20:32:00 +0200

    let key => const key

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,6 +1,6 @@
 function sumSalary(salaries) {
-  const sum = 0;
-  for (let key in salaries) {
+  let sum = 0;
+  for (const key in salaries) {
     if (typeof salaries[key] == 'number' && isFinite(salaries[key])) {
       sum += salaries[key];
     }
