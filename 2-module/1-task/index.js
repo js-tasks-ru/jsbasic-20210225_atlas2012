155ace8706fecc7010ef2bf7b6725083d20036b9
Serghei Nezdvetki <moeapo4ta@gmail.com>
2021-03-10 20:26:18 +0200

    variable replacement

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,5 +1,5 @@
 function sumSalary(salaries) {
-  let sum = 0;
+  const sum = 0;
   for (let key in salaries) {
     if (typeof salaries[key] == 'number' && isFinite(salaries[key])) {
       sum += salaries[key];
